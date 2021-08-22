c34fcc63faf820d5b5cdb70ea50d80c9cc5e7c0c
Djlevd <81247733+Djlevd@users.noreply.github.com>
2021-08-22 14:36:15 +0200

    Update ngrok-user.bat

--- resources/ngrok-user.bat
+++ resources/ngrok-user.bat
@@ -4,6 +4,6 @@ sc start audiosrv >nul
 echo All done! Connect your VM using RDP.
 echo RDP By Avishkar Patil
 echo IP:
-echo Username: runneradmin
+echo Username: administrator
 echo Password: djodjo0123
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo NGROK Services is not Running
