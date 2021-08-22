31f225446b91514b02292309bba6f6e9e94608d5
Djlevd <81247733+Djlevd@users.noreply.github.com>
2021-08-22 14:42:03 +0200

    Delete resources directory

--- resources/ngrok-user.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-diskperf -Y >nul
-sc start audiosrv >nul
-echo All done! Connect your VM using RDP.
-echo RDP By Avishkar Patil
-echo IP:
-echo Username: administrator
-echo Password: djodjo0123
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo NGROK Services is not Running
